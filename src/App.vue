eeeecc430a5507fda7e8caf0cc9d55d56212d511
HironoriSato <maza.hs5360@gmail.com>
2020-02-27 22:45:42 +0900

    [mod]forSale, inCartをgettersから呼び出すよう修正

--- src/App.vue
+++ src/App.vue
@@ -13,31 +13,25 @@
           :image="item.image"
         />
       </b-row>
+      {{ inCart }}
     </b-container>
   </b-container>
 </template>
 
 <script>
 import Item from "@/components/Item";
+import { mapGetters } from "vuex";
 
 export default {
   name: "app",
   data() {
-    return {
-      forSale: [
-        { invId: 1, name: "An Item", image: "//placehold.it/200", price: 999 },
-        { invId: 2, name: "Thing", image: "//placehold.it/200", price: 1499 },
-        { invId: 3, name: "Doo-dad", image: "//placehold.it/200", price: 499 },
-        {
-          invId: 4,
-          name: "Other thing",
-          image: "//placehold.it/200",
-          price: 299
-        }
-      ]
-    };
+    return {};
   },
-  components: { Item, }
+  components: { Item },
+  computed: {
+    // forSale() { return this.$store.getters.forSale; }
+    ...mapGetters(["forSale", "inCart"])
+  },
 };
 </script>
 
