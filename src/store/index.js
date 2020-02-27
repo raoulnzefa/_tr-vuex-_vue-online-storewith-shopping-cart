c0024ce6972c6cb89a80e1a7facb83cc63fb1857
HironoriSato <maza.hs5360@gmail.com>
2020-02-27 22:45:07 +0900

    [add]Itemに対してのstate, mutations, getters, actionsを作成

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    forSale: [
+      { invId: 1, name: "An Item", image: "//placehold.it/200", price: 999 },
+      { invId: 2, name: "Thing", image: "//placehold.it/200", price: 1499 },
+      { invId: 3, name: "Doo-dad", image: "//placehold.it/200", price: 499 },
+      {
+        invId: 4,
+        name: "Other thing",
+        image: "//placehold.it/200",
+        price: 299
+      }
+    ],
+    inCart: []
+  },
+  getters: {
+    // forSale: function (state) {
+    //   return state.forSale;
+    // },    
+    forSale: state => state.forSale,
+    inCart: state => state.inCart,
   },
   mutations: {
+    ADD_TO_CART (state, invId) { state.inCart.push(invId); }
   },
   actions: {
+    addToCart (context, invId) { context.commit('ADD_TO_CART', invId) }
   },
   modules: {
   }
