d770ae0dc108f114bc9258a0d47c64d8eb14e774
HironoriSato <maza.hs5360@gmail.com>
2020-02-27 22:14:55 +0900

    [mod]App.vueを実装

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <b-container id="app" fluid>
+    <h1 class="my-3">My online store</h1>
+
+    <b-container>
+      <b-row>
+        <b-col md="3" v-for="(item, i) in forSale" :key="i">
+          <b-card :img-src="item.image" img-top>
+            <b-card-title>{{ item.name }}</b-card-title>
+            <b-card-text>${{ item.price/100 }}</b-card-text>
+            <div class="text-right">
+              <b-button variant="primary" size="sm">Add to card</b-button>
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-container>
 </template>
 
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      forSale: [
+        { invId: 1, name: "An Item", image: "//placehold.it/200", price: 999 },
+        { invId: 2, name: "Thing", image: "//placehold.it/200", price: 1499 },
+        { invId: 3, name: "Doo-dad", image: "//placehold.it/200", price: 499 },
+        {
+          invId: 4,
+          name: "Other thing",
+          image: "//placehold.it/200",
+          price: 299
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
