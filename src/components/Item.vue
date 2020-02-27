68c2c1c9e5d1b9dff46b8b834e852068bb750a2a
HironoriSato <maza.hs5360@gmail.com>
2020-02-27 22:46:02 +0900

    [add]addToCartメソッドを実装

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -6,7 +6,7 @@
         <b-card-text>${{ price|dollars }}</b-card-text>
       </b-card-body>
       <div class="text-right">
-        <b-button variant="primary" size="sm">Add to cart</b-button>
+        <b-button variant="primary" size="sm" @click="addToCart(invId)">Add to cart</b-button>
       </div>
     </b-card>
   </b-col>
@@ -18,6 +18,11 @@ export default {
   props: ["name", "price", "image", "invId"],
   filters: {
     dollars: num => `$${num / 100}`
+  },
+  methods: {
+    addToCart(invId) {
+      return this.$store.dispatch("addToCart", invId);
+    }
   }
 };
 </script>
